f5cdcf7a5ddf0ae655a8213174c755ae9abe3291
Ruckapark <georgeadruck@gmail.com>
2023-02-21 17:27:14 +0100

    begin video crops

---
+++ crop_videos.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 21 17:17:17 2023
+
+Crop video automatically using timestamp function
+
+@author: George
+"""
+
+#imports
+
+
+if __name__ == '__main__':
+    
+    #bring in video on 10 minutes
+    
+    #extract first and last frame using ffmpeg
+    
+    #add ffmpeg commands to readme
+    
+    #defin dopage to be in the middle
+    
+    #calculate timedelta beween final frame and dopage
+    
+    #use ffmpeg to crop to new shorter version of video
+    
+    #delete original video and rename new video
+    
+    #TRIPLE check this works.
