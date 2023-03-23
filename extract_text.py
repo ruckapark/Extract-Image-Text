3d8a6c0a607dc33a8bdea9bf0feb00785c8f6c82
Ruckapark <georgeadruck@gmail.com>
2023-03-23 09:46:43 +0100

    Workaround for / character poorly recognised by Tesseract

--- extract_text.py
+++ extract_text.py
@@ -22,13 +22,29 @@ def strptime_backup2(string):
 def strptime_backup3(string):
     return datetime.strptime(string, "%d%m%Y %H:%M:%S")
 
+def strptime_backup4(string):
+    if len(string.split(' ')[0]) == 10:
+        string = string[:2] + '/' + string[3:]
+        string = string[:5] + '/' + string[6:]
+        return strptime_(string)
+    elif len(string.split(' ')[0]) == 9:
+        if string[2] not in ['0','1']:
+            string = string[:2] + '/' + string[3:]
+            return strptime_backup1(string)
+        else:
+            string = string[:4] + '/' + string[5:]
+            return strptime_backup2(string)
+    else:
+        print('String no recognised')
+
 def strptime(string):
     
     strps = [
         strptime_,
         strptime_backup1,
         strptime_backup2,
-        strptime_backup3]
+        strptime_backup3,
+        strptime_backup4]
     
     for strp in strps:
         try:
@@ -78,7 +94,7 @@ if __name__ == '__main__':
     #here you should download the tesseract binaries, do not use the pytesseract.exe wrapper
     pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract.exe'    
     
-    img = cv2.imread('20201120-093425_Erpo_0001_crop_last.jpg',0)
+    img = cv2.imread('20201211-080603_Erpo_0001_last.jpg',0)
     
     #portion of interest
     cropped_img = np.flip(np.transpose(img[0:140,944:964]),0)
